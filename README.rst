37dfdc6311099f855b47e74836a90b29d5c26303
Russell Owen <rowen@uw.edu>
2021-09-01 16:17:15 -0700

    Initial files

---
+++ README.rst
@@ -0,0 +1,8 @@
+########
+ts_utils
+########
+
+Python utilities used by ts_salobj and other Telescope and Site software.
+The main focus is TAI time (functions such as current_tai and tai_from_utc) and angles (functions such as angle_diff and angle_wrap_center).
+
+This package has few dependencies beyond AstroPy.
---
+++ doc/conf.py
@@ -0,0 +1,16 @@
+"""Sphinx configuration file for an LSST stack package.
+
+This configuration only affects single-package Sphinx documentation builds.
+For more information, see:
+https://developer.lsst.io/stack/building-single-package-docs.html
+"""
+
+from documenteer.conf.pipelinespkg import *
+import lsst.ts.utils  # noqa
+
+project = "ts_utils"
+html_theme_options["logotext"] = project  # type: ignore # noqa
+html_title = project
+html_short_title = project
+# Avoid warning: Could not find tag file _doxygen/doxygen.tag
+doxylink = {}  # type: ignore
